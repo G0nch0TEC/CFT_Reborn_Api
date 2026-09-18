7b083a7b1d7386cf03eec4d9dc81b9008ca8374f
G0nch0TEC <benzaquenrios@gmail.com>
2026-09-18 12:53:13 -0500

    Avanzando
    @Columns, etc en Categoria y producto

--- src/main/java/Reborn_backend/Backend/domain/entities/Categoria.java
+++ src/main/java/Reborn_backend/Backend/domain/entities/Categoria.java
@@ -1,4 +1,22 @@
 package Reborn_backend.Backend.domain.entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_CAT", nullable = false)
+    private Integer idcat;
+
+    @Column(name = "nombre", length = 50)
+    private String nombre;
+
+    @Column(name = "estado", nullable = false)
+    private Byte estado = 1;
+
+    @OneToMany(mappedBy = "categoria", fetch = FetchType.LAZY)
+    private List<Producto> productos;
 }
--- src/main/java/Reborn_backend/Backend/domain/entities/Producto.java
+++ src/main/java/Reborn_backend/Backend/domain/entities/Producto.java
@@ -1,4 +1,29 @@
 package Reborn_backend.Backend.domain.entities;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "producto")
 public class Producto {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Categoria", nullable = false)
+    private Categoria categoria;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "KeyProduct", nullable = false)
+    private Integer keyproduct;
+
+    @Column(name = "Nombre", nullable = false, length = 80)
+    private String nombre;
+
+    @Column(name = "Descripcion", length = 150)
+    private String descripcion;
+
+    @Column(name = "Precio", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @Column(name = "estado", nullable = false)
+    private Byte estado = 1;
 }
