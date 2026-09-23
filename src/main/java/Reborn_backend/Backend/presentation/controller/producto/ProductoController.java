763e3b8cd8850dd21c244dfdeb9fe4dd71e411d8
G0nch0TEC <benzaquenrios@gmail.com>
2026-09-23 09:16:34 -0500

    ProductoController: CrearProducto

--- src/main/java/Reborn_backend/Backend/presentation/controller/producto/ProductoController.java
+++ src/main/java/Reborn_backend/Backend/presentation/controller/producto/ProductoController.java
@@ -1,4 +1,41 @@
 package Reborn_backend.Backend.presentation.controller.producto;
 
+import Reborn_backend.Backend.domain.dto.request.producto.ProductoRequest;
+import Reborn_backend.Backend.domain.dto.response.producto.ProductoResponse;
+import Reborn_backend.Backend.domain.entities.Producto;
+import Reborn_backend.Backend.domain.use_case.producto.CrearProductoCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producto")
 public class ProductoController {
+    private final CrearProductoCase crearProductoCase;
+
+    public ProductoController(CrearProductoCase crearProductoCase){
+        this.crearProductoCase = crearProductoCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductoResponse> crearProducto(@RequestBody ProductoRequest productoRequest){
+
+        Producto producto = new Producto();
+        producto.setNombre(productoRequest.getNombre());
+        producto.setDescripcion(productoRequest.getDescripcion());
+        producto.setPrecio(productoRequest.getPrecio());
+
+        Producto productoGuardado = crearProductoCase.crearProducto(producto, productoRequest.getIdCategoria());
+
+        ProductoResponse productoResponse = new ProductoResponse(
+                productoGuardado.getKeyproduct(),
+                productoGuardado.getNombre(),
+                productoGuardado.getDescripcion(),
+                productoGuardado.getPrecio()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoResponse);
+    }
 }
